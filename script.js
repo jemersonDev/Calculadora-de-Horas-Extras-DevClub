b105e5ec469ac46ebcf73f66595496ed4e722ce1
jemerson03 <jemersontham@gmail.com>
2026-02-28 14:17:54 -0300

    corrigindo error

--- script.js
+++ script.js
@@ -1,24 +1,23 @@
 function calcular() {
-    // 1. Pegamos os elementos do HTML
+   
     const inputSalario = document.getElementById("salario");
-    const inputQuantidade = document.getElementById("quantidade"); // Pegando a quantidade
+    const inputQuantidade = document.getElementById("quantidade"); 
     const displayResultado = document.getElementById("resultado");
 
-    // 2. Transformamos os textos em números
+    
     let salario = parseFloat(inputSalario.value);
-    let qtdHoras = parseFloat(inputQuantidade.value);
-
-    // 3. Verificamos se os valores são válidos
+    let qtdHoras = parseFloat(inputQuantidade.value); 
+   
     if (isNaN(salario) || salario <= 0) {
         alert("Por favor, digite um valor válido para o salário!");
         return;
     }
 
     if (isNaN(qtdHoras) || qtdHoras <= 0) {
-        qtdHoras = 1; // Se a pessoa não digitar horas, calculamos para 1h por padrão
+        qtdHoras = 1; 
     }
 
-    // 4. Fazemos os cálculos (Tudo dentro da função calcular!)
+    
     let valorHoraComum = salario / 220;
     const taxas = [100, 75, 65, 50, 40];
     let conteudoHtml = "";
@@ -41,6 +40,6 @@ function calcular() {
         `;
     });
 
-    // 5. Colocamos o resultado na tela
+    
     displayResultado.innerHTML = conteudoHtml;
-} // <--- A CHAVE SÓ FECHA AQUI NO FINAL!
+}
